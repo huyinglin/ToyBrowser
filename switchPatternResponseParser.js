80776b350a42fe38161b829a166219c1702aaf8a
huyinglin <huyinglinfz@gmail.com>
2020-05-13 14:53:10 +0800

    feat: update Transfer-Encoding info

--- switchPatternResponseParser.js
+++ switchPatternResponseParser.js
@@ -61,6 +61,19 @@ class ResPonseParser {
         return this.waitingHeaderSpace;
       case '\r':
         if (this.headers['Transfer-Encoding'] === 'chunked') {
+          // Transfer-Encoding: 指明了将实体传递给用户所采用的编码形式。
+          //
+          // @Syntax
+          // chunked: 数据以一系列分块的形式进行发送。Content-Length 首部在这种情况下不被发送。
+          //   在每一个分块的开头需要添加当前分块的长度，以十六进制的形式表示，后面紧跟着 '\r\n' ，
+          //   之后是分块本身，后面也是'\r\n' 。终止块是一个常规的分块，不同之处在于其长度为0。
+          //   终止块后面是一个挂载（trailer），由一系列（或者为空）的实体消息首部构成。
+          // compress: 采用 Lempel-Ziv-Welch 压缩算法。这种内容编码方式已经被大部分浏览器弃用，部分因为专利问题（这项专利在2003年到期）。
+          // deflate: 采用 zlib 结构 (在 RFC 1950 中规定)，和 deflate 压缩算法(在 RFC 1951 中规定)。
+          // gzip: 表示采用 Lempel-Ziv coding (LZ77) 压缩算法，以及32位CRC校验的编码方式。
+          // identity: 用于指代自身（例如：未经过压缩和修改）。除非特别指明，这个标记始终可以被接受。
+          //
+          // gzip, chunked: 可以是多个值，多个值之间以逗号分隔
           this.bodyParser = new TrunkedBodyParser();
         }
         return this.waitingHeaderBlockEnd;
